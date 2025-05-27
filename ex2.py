c06437942d7c4abe8db69e7994b7a62d829b2c19
e2400096 <chloesondag.pro@gmail.com>
2025-05-27 11:26:20 +0200

    td2

--- ex2.py
+++ ex2.py
@@ -10,11 +10,29 @@ c : capital acquis
 ci : capital acquis avec intérêts
 csi : capital acquis sans intérêts
 """
-
-print('Calcul du capital acquis et de ses intérêts versé lorsque intérêts sont calculés une fois par an.')
+#exercice 1
+print('Calcul du capital acquis et de ses intérêts versés lorsque intérêts sont calculés une fois par an.')
 p = int(input('Entrer le placement de départ :'))
 m = int(input('Entrer le montant du versement mensuel :'))
-t = int(input('Entrer le taux annuel :'))
+t = float(input('Entrer le taux annuel en % :'))
+n = int(input('Entrer le nombre d\'années :'))
+
+c = p
+i = 0
+for j in range (n):
+    c = ((m*12) + c) * (1+t/100)
+    i = c - p
+    csi = ((m*12) + c) - p
+print(f'Le capital acquis avec intérêts est de {round(c,2)} euros au bout de {n} ans avec des versements mensuels de {m} euros.')
+print(f'Les intérêts gagnés avec taux annuels de {t} % sont de {round(i,2)} euros.')
+print(f'Sans placements avec intérêts, le capital acquis serait de {round(csi,2)} euros.')
+
+"""
+#exercice 2
+print('Calcul du capital acquis et de ses intérêts versés lorsque intérêts sont calculés une fois par mois.')
+p = int(input('Entrer le placement de départ :'))
+m = int(input('Entrer le montant du versement mensuel :'))
+t = int(input('Entrer le taux annuel en % :'))
 n = int(input('Entrer le nombre d\'années :'))
 
 for j in range ():
@@ -23,3 +41,4 @@ for j in range ():
 print(f'Le capital acquis avec intérêts est de {} euros au bout de {n} ans avec des versements mensuels de {m} euros.')
 print(f'Les intérêts gagnés avec taux annuels de {t} % sont de {} euros.')
 print(f'Sans placements avec intérêts, le capital acquis serait de {} euros.')
+"""
