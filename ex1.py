abb83da7f6d85a8fe1f8e9940cb416de1046dfe1
e2400096 <chloesondag.pro@gmail.com>
2025-05-27 10:47:00 +0200

    td2

--- ex1.py
+++ ex1.py
@@ -8,12 +8,12 @@ tm = t/12/100
 a = (1+tm)**(12*n)
 m = s*tm*a/(a-1)
 b = n*12
-
-inte1 = s*tm
-cr = tm - interet
+#brouillon
+#inte1 = s*tm
+#cr = tm - interet
 print(f'La mensualité est de {tm} euros.')
 print(f'La mensualité avec intérêts est de {round(m,2)} euros.')
-print(f'Le montant des intérêts remboursés sont de {round(m*12n-s,2)} euros.')
+print(f'Le montant des intérêts remboursés sont de {round(m*12*n-s,2)} euros.')
 #print(f'Le montant des intérêts remboursés sont de {a} euros.')
 #print(f'Le taux mensuel est de {m} euros.')
 print(f'Le taux mensuel est de {tm} euros.')
@@ -22,16 +22,15 @@ print('\nTableau d\'amortissement:')
 print('Mois - Mensualité - Intérêts - Capital remboursé - Capital restant - Intérêts remboursés')
 
 #version du prof
-ir=0
-for i in (b):
-    ir = 0
+ir=0 #!! attention à pas oublier ça
+for j in range (n*12):
     i = s*tm
     cr = m-i
-    crd=s-cr
+    crd = s-cr
     ir = i + ir
 
-    print(f'{i+1} - {round(m,1)} - {round(i,1)} - {round(cr,1)} - {round(crd, 1)} - {round(ir,2)}')
-    s=crd
+    print(f'{j+1} - {round(m,1)} - {round(i,1)} - {round(cr,1)} - {round(crd, 1)} - {round(ir,2)}')
+    s=crd #!! ligne très importante pour la récursivité
 
 """
 #version brouillon
